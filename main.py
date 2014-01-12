34c35c903293ae53756fb78f87e02622d3ffe114
vaseem-khan <vakhan93@gmail.com>
2014-01-12 21:45:00 +0530

    functions updated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import jinja2
 import urllib2
 import re
 from google.appengine.ext import db
-
+import wiki
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
@@ -27,74 +27,57 @@ class BaseHandler(webapp2.RequestHandler):
 
 params=dict()
 params.clear()
+
+# Api keys
+CB_API_KEY="Enter Crunchbase Api key here"
+GMAP_API_KEY="Enter Gmaps Api key here"
+YouTube_API_KEY="Enter Youtube Api key here"
+
+
 #duck duck go
 def topic_summary(w):
     k=str(w)
     params["Keyword"]=k
     l=k.lower()
     t=l.replace (" ", "+")        
-    link="http://api.duckduckgo.com/?q="+t+"&format=json&pretty=1&no_html=1"
-    f=[]
-    d=[]
+    link="http://api.duckduckgo.com/?q="+t+"&format=json&no_html=1"
     try:
         req = urllib2.Request(link, None)
         f = opener.open(req)
-        d=json.load(f)
-    except:
-        return None
+        duck=json.load(f)
 
-    ret=""
-    if d["Answer"]!="":
-        params["gudies"]= d["Answer"]
-    else:
-        params["gudies"]= None
-        
-    if d["Definition"]=="":
-        params["duck_defination"]=None
-    else:
-        params["duck_defination"]= d["Definition"]
-    if d["AbstractText"]=="":
-        params["duck_abstract"]=None        
-    else:
-        params["duck_abstract"]=d["AbstractText"]
-    length= len(d["RelatedTopics"])
-    i=0;
-    params['duck_image']=None
-    if  d["Image"]!="":
-        params['duck_image']=d["Image"]
-    else:
-         params['duck_image']=None
-    try:
-        if d["RelatedTopics"][0]["Icon" ]["URL"]!="":
-            params['duck_image']=d["RelatedTopics"][0]["Icon" ]["URL"]        
+        for key in duck.keys():
+            if type(duck[key])!=list:
+                params["duck_"+str(key)]=duck[key]
+        params["RelatedTopics"]=[]
+        flag=False
+        duck_rel={}
+        duck_rel["type"]="Similar"
+        duck_rel["lis"]=[]
+        for obj in duck["RelatedTopics"]:
+            if "Name" not in obj.keys():
+                flag=True
+                duck_rel["lis"].append(obj)
+        if flag:
+            params["RelatedTopics"].append(duck_rel)
+
+        for obj in duck["RelatedTopics"]:
+            duck_rel={}
+            if "Name" in obj.keys():
+                flag=True
+                duck_rel["type"]=obj["Name"]
+                duck_rel["lis"]=[]
+                for it in obj["Topics"]:
+                    duck_rel["lis"].append(it)
+                params["RelatedTopics"].append(duck_rel)
+
+        if not(flag):
+            params["error_"]="ahh !! Keyword not found try another one"
+
     except:
-        params['duck_image']=None
-    
-    params["duck_related"]=[]
-    if length<10:
-        for i in range(1,length):
-            if "Result" in d["RelatedTopics"][i] and d["RelatedTopics"][i]["Text"]!="":
-                params["duck_related"].append(d["RelatedTopics"][i]["Text"])
-            elif  'Topics' in d["RelatedTopics"][i]:
-                l2=len(d["RelatedTopics"][i]["Topics"])
-                k=0
-                while k<l2 and i<11:
-                    if  "Result" in d["RelatedTopics"][i]["Topics"][k] and  d["RelatedTopics"][i]["Topics"][k]["Text"]!="":
-                        params["duck_related"].append(d["RelatedTopics"][i]["Topics"][k]["Text"])
-                    k+=1
-                    
-    else:
-        for i in range(1,10):
-            if "Result" in d["RelatedTopics"][i] and d["RelatedTopics"][i]["Text"]!="":
-                params["duck_related"].append(d["RelatedTopics"][i]["Text"])
-            elif  'Topics' in d["RelatedTopics"][i]:
-                l2=len(d["RelatedTopics"][i]["Topics"])
-                k=0
-                while k<l2 and k+i<11:
-                    if  "Result" in d["RelatedTopics"][i]["Topics"][k] and d["RelatedTopics"][i]["Topics"][k]["Text"]!="" :
-                        params["duck_related"].append(d["RelatedTopics"][i]["Topics"][k]["Text"])
-                    k+=1
-    return params
+        params["error_"]="Oops something went wrong"
+
+
 def html_to_text(data):        
     # remove the newlines
     data = data.replace("\n", " ")
@@ -112,23 +95,22 @@ def html_to_text(data):
 #CrunchBase
 def cb(term):
     term=term.replace(" ", "-")
-    link="http://api.crunchbase.com/v/1/company/"+term+".js"
+    link="http://api.crunchbase.com/v/1/company/"+term+".js?api_key="+CB_API_KEY
+    print link
     try:
         req = urllib2.Request(link, None)
         f = opener.open(req)
-        d=json.load(f)
+        d=json.load(f)       
+
+        params["cb_overview"]=html_to_text(d['overview'])
+        params["cb_name"]=d['name']
+        params["cb_homeurl"]=d['homepage_url']
+        params["cb_url"]=d['crunchbase_url']
+        params["cb_category"]=d['category_code']
+
     except:
         params["cb_name"]=None
-        return None
-    if 'error' in d:
-        params["cb_name"]=None
-        return None
-    else:
-        params["cb_overview"]=(d['overview'])
-        params["cb_name"]=d['name']
-        params["cb_url"]=d['homepage_url']
-        params["cb_category"]=d['category_code']
-        return params
+
 
 #Twitter
 def twit(term):
@@ -150,11 +132,11 @@ def twit(term):
         for i in range(1,10):
              params["twitter"+str(i)]=d['results'][i]['text']
     #params["twitter"]=answer
-    return params
 
 #IMDB
 def imdb(term):
     term=term.replace(" ","+")
+  
     link="http://www.omdbapi.com/?s="+term
     try:
         req = urllib2.Request(link, None)
@@ -163,39 +145,23 @@ def imdb(term):
         links=[]
         for i in lis["Search"]:
             if i["imdbID"]:
-                 links.append("http://www.omdbapi.com/?i="+i["imdbID"])
-        link=links[0]
-        req = urllib2.Request(link, None)
-        f = opener.open(req)
-        imdb =json.load(f)
-        params["imdb_Title"]=imdb["Title"]
-        params["imdb_Year"]=imdb["Year"]
-        params["imdb_Rated"]=imdb["Rated"]
-        params["imdb_Released"]=imdb["Released"]
-        params["imdb_Runtime"]=imdb["Runtime"]
-        params["imdb_Genre"]=imdb["Genre"]
-        params["imdb_Director"]=imdb["Director"]
-        params["imdb_Writer"]=imdb["Writer"]
-        params["imdb_Actors"]=imdb["Actors"]
-        params["imdb_Plot"]=imdb["Plot"]
-        params["imdb_Language"]=imdb["Language"]
-        params["imdb_Country"]=imdb["Country"]
-        params["imdb_Awards"]=imdb["Awards"]
-        params["imdb_Poster"]=imdb["Poster"]
-        params["imdb_Metascore"]=imdb["Metascore"]
-        params["imdb_imdbRating"]=imdb["imdbRating"]
-        params["imdb_imdbVotes"]=imdb["imdbVotes"]
-        params["imdb_imdbID"]=imdb["imdbID"]
-        params["imdb_Type"]=imdb["Type"]
-        return params
+                 links.append("http://www.omdbapi.com/?i="+i["imdbID"])      
+        l=min(len(links),4)
+       
+
+        params["imdb"]=[]
+        for i in range(l):
+            req = urllib2.Request(links[i], None)
+            f = opener.open(req)
+            imdb =json.load(f)
+            params["imdb"].append(imdb);
     except:
-        params['imdb_title']=None
-        return params
+        params['imdb']=None
 
 #gmaps
 def gmaps(term):
     loc=term.replace(" ","+")
-    link='http://maps.googleapis.com/maps/api/staticmap?center='+loc+'&zoom=12&size=600x300&maptype=roadmap&sensor=false&key=AIzaSyBAspYdcOeGNE_gKhbLtOTrIUwqKByrJ9M'
+    link='http://maps.googleapis.com/maps/api/staticmap?center='+loc+'&zoom=12&size=600x300&maptype=roadmap&sensor=false&key='+GMAP_API_KEY
     try:
         infile = opener.open(link)
         page = infile.read()
@@ -215,7 +181,7 @@ def faceb(term):
         d=json.load(f)
         if 'error' in d:
             params['facebook_url']=None
-            return None
+           
         elif 'picture' in d:
             #return d['picture']+"<br>"+d['link']+"<br>"+d['about']+"<br>"+str(d['likes'])
             params['facebook_name']=term
@@ -223,58 +189,18 @@ def faceb(term):
             params['facebook_url']=d['link']
             params['facebook_about']=d['about']
             params['facebook_likes']=str(d['likes'])
-            return params
+            
         else:
             #return d['link']+"<br>"+d['about']+"<br>"+str(d['likes'])
             params['facebook_name']=term
             params['facebook_url']=d['link']
             params['facebook_about']=d['about']
             params['facebook_likes']=str(d['likes'])
-            return params
+            
     except:
         params['facebook_url']=None
-        return None
-
-#TechIRIS
-def iris(term):
-    term=term.replace(" ", "")
-    link="http://thetechiris.com/api/get_search_results/?dev=1&search="+term
-    try:
-        req = urllib2.Request(link, None)
-        f = opener.open(req)
-        d=json.load(f)
-    except:
-        return None
-    if d['count']==0:
-        return None
-    elif d['count']>=2:
-        params["ti_post0_title"]=d['posts'][0]['title']
-        params["ti_post0_url"]=d['posts'][0]['url']
-        params["ti_post1_title"]=d['posts'][1]['title']
-        params["ti_post1_title"]=d['posts'][1]['url']
-        return params
-    else:
-        params["ti_post0_title"]=d['posts'][0]['title']
-        params["ti_post0_url"]=d['posts'][0]['url']
-        return params
+        
     
-#Free dictionary
-def fdictionary(w):
-    k=str(w);
-    l=k.lower()
-    t=l.replace (" ", "+")        
-    link="http://api.duckduckgo.com/?q=define+"+t+"&format=json&pretty=1"
-    try:
-        req = urllib2.Request(link, None)
-        f = opener.open(req)
-        d=json.load(f)
-    except:
-        return None
-    if d["Abstract"]!="":
-        #return d["Abstract"]
-        params["freeDict"]=d["Abstract"]
-        return params
-
 #news
 def news(term):
     t=[]
@@ -282,50 +208,49 @@ def news(term):
     term=term.replace(" ", "-")
     link="http://news.google.com/news?q="+term+"&output=rss"
     try:
-       from xml.dom.minidom import parseString
-       file = urllib2.urlopen(link)
-       data = file.read()
-       file.close()
-       dom = parseString(data)
+        from xml.dom.minidom import parseString
+        file = urllib2.urlopen(link)
+        data = file.read()
+        file.close()
+        dom = parseString(data)   
+        for i in dom.getElementsByTagName('title'):
+            xmlTag = i.toxml()
+            t.append((xmlTag.replace('<title>','').replace('</title>','')))
+
+        for i in dom.getElementsByTagName('link'):
+            xmlTag1 = i.toxml()
+            l.append((xmlTag1.replace('<link>','').replace('</link>','')))
+
+        params["newsitem"]=[]
+        if len(l)>2:
+            for i in xrange(2,len(l)):
+                news_dict={}
+                news_dict["content"]=t[i]
+                
+                Istart=l[i].find("url=")
+                if Istart!=-1:
+                    news_dict["link"]=(l[i][Istart+4:])
+                params["newsitem"].append(news_dict)
+                if i==15:
+                    break;
     except:
-        return None
-   
-    for i in dom.getElementsByTagName('title'):
-        xmlTag = i.toxml()
-        t.append((xmlTag.replace('<title>','').replace('</title>','')))
-    
-    for i in dom.getElementsByTagName('link'):
-        xmlTag1 = i.toxml()
-        l.append((xmlTag1.replace('<link>','').replace('</link>','')))
-  
-    params["newsitem"]=[]
-    params["newslink"]=[]
-    if len(l)>2:
-        for i in xrange(2,len(l)):
-            params["newsitem"].append(t[i])
-            Istart=l[i].find("url=")
-            if Istart!=-1:
-                params["newslink"].append(l[i][Istart+4:])
-            if i==6:
-                break;
-    return params
+        params["newsitem"]=None
+
 
 
 #quora
 def quora(w):
     k=str(w)
     l=k.lower()
-    t=l.replace (" ", "+")        
-    #return "http://www.quora.com/search?q="+t+"&context_type=&context_id="
-    params["quora"]= "http://www.quora.com/search?q="+t+"&context_type=&context_id="
-    return params
+    t=l.replace (" ", "+")
+    params["quora"]= "http://www.quora.com/search?q="+t
 
 
 #Youtube
 def youtube(term):
     term=term.replace(" ","+")
     params["yt"]=[]
-    link="https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=AIzaSyCJoYbm7RWiP4uEp5wlKp2WN7ptQ2F2z_g&q="+term
+    link="https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key="+YouTube_API_KEY+"&q="+term
     try:
         req = urllib2.Request(link, None)
         f = opener.open(req)
@@ -339,10 +264,10 @@ def youtube(term):
             vid["description"]=it["snippet"]["description"]
             vid["thumbnails"]=it["snippet"]["thumbnails"]["medium"]["url"]
             params["yt"].append(vid)
-        return params
+       
     except:
         params["yt"]=None
-        return None;
+        
     
 class MainPage(BaseHandler):
     def get(self):
@@ -353,47 +278,27 @@ class MainPage(BaseHandler):
         
         #params = dict(q=q)
         qs=q.strip()
-        p=topic_summary(qs)
-        p2=cb(qs)
-        p3=twit(qs)
-        p4=imdb(qs)
+        topic_summary(qs)
+        cb(qs)
+        
+        txt=wiki.extractwiki(qs)
+        if txt!=None:
+            params["wiki_"]=txt
+        else:
+            params["wiki_"]=None
+        # p3=twit(qs)
+        imdb(qs)
         # img_url=gmaps(qs)
-        p5=faceb(qs)
-        p6=iris(qs)
-        p7=fdictionary(qs)
-        p8=quora(qs)
-        p9=youtube(qs)
-        p10=news(qs)
+        faceb(qs)
+        # p6=iris(qs)
+        quora(qs)
+        youtube(qs)
+        news(qs)
                
-        """if p:
-            self.write("<h1> Results wiki</h1>"+p)
-        if p10:
-            self.write('<br><h1>News</h1> '+p10)
-        if p2:
-            self.write("<br><br><h1> Results CB</h1>"+p2)
-        if p3:
-            self.write("<br><br><h1> Results Twitter</h1>"+p3)
-        if p4:
-            #self.write("<br><br><h1> Results IMDB</h1>"+p4)"""
+        
         self.render('results.html', **params)
+
         # if img_url:
         #     self.write('<br><br><h1> Results maps</h1>'+ '<img src="'+img_url+'" >')
-        """if p5:
-            self.write("<br><br><h1> Results Fb</h1>"+p5)
-            self.render('results.html', **fbparams)
-        if p6:
-            self.write("<br><br><br><br><br><br><br><br<br><br><br><br><br><br><h1> Results TechIRIS</h1>"+p6)
-        if p7:
-            self.write("<br><br><h1>Free Dictionary </h1>"+p7)
-        if p8:
-            self.write('<br><h3>search on quora <a href="'+p8+'">'+q+'</a><h3>')
-        if p9:
-            self.write('<br><h3>search on youtube <a href="'+p9+'">'+q+'</a><h3>')"""
-        
-
-        # if not p and not p2 and not p3 and not p4 and not img_url and not p5 and not p6 and not p7 and not p8 and not p9:
-        #     params['error_q']="That's not a valid term!"
-        #     self.render('search-form.html', **params)    
-
         
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
